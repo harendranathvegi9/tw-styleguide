389cf19377af6fae6e0416158b1dcdf615775dfe
nisheedj <nisheedj@thoughtworks.com>
2015-06-22 13:10:21 +0530

    [Nisheed] Styleguide updated

--- gulpfile.js
+++ gulpfile.js
@@ -12,19 +12,24 @@ var exec = require('child_process').exec;
 gulp.task('clean:build', function(cb) {
   del(['./build', './styleguide'], cb);
 })
+//Copy template folder
+.task('tempate:copy', ['clean:build'], function() {
+  return gulp.src('src/tw-template/**')
+    .pipe(gulp.dest('build/template'));
+})
 //compile less file
-.task('less', ['clean:build'], function() {
+.task('template:css', ['clean:build', 'tempate:copy'], function() {
   return gulp.src('src/less/thoughtworks.less')
     .pipe(less())
-    .pipe(gulp.dest('build/css'))
+    .pipe(gulp.dest('build/template/public'))
     .pipe(minify())
     .pipe(rename(function(path) {
       path.basename += '.min';
     }))
-    .pipe(gulp.dest('build/css'));
+    .pipe(gulp.dest('build/template/public'));
 })
 //Create styleguide
-.task('styleguide:create', ['clean:build', 'less'], function(cb) {
+.task('styleguide:create', ['clean:build', 'tempate:copy', 'tempate:assets', 'template:css'], function(cb) {
   exec('"./node_modules/.bin/kss-node" --config "./kss-config.json"', function(err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
@@ -32,9 +37,9 @@ gulp.task('clean:build', function(cb) {
   });
 })
 //Copy styleguide assets
-.task('styleguide:copy', ['styleguide:create'], function() {
-  return gulp.src(['./build/css/**', './src/img/**', './src/js/**'])
-    .pipe(gulp.dest('./styleguide/public'));
+.task('tempate:assets', ['tempate:copy'], function() {
+  return gulp.src(['./src/img/**', './src/js/**'])
+    .pipe(gulp.dest('./build/template/public'));
 })
 //Generate the styleguide
-.task('styleguide', ['styleguide:create', 'styleguide:copy']);
+.task('styleguide', ['styleguide:create']);
